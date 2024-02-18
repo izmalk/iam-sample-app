c559430df037e1c036224e429aee7fed4a3b1852
Vladimir Izmalkov <vladimir@vaticle.com>
2024-02-18 23:41:51 +0000

    Gradual update

--- sample.py
+++ sample.py
@@ -1,54 +1,110 @@
 from typedb.driver import TypeDB, SessionType, TransactionType, TypeDBOptions
-from datetime import datetime
-
-import queries
 import setup
 
-DB_NAME = "iam"
+DB_NAME = "sample_app"
 SERVER_ADDR = "127.0.0.1:1729"
 
 
-if __name__ == "__main__":
-    print("IAM Sample App")
-    if setup.db_setup():
-        print("Commencing sample requests.")
+def fetch_all_users(driver):
+    print("\nRequest #1: Fetching all users as JSON objects with full names and emails")
+    with driver.session(DB_NAME, SessionType.DATA) as data_session:
+        with data_session.transaction(TransactionType.READ) as read_tx:
+            users = read_tx.query.fetch("match $u isa user; fetch $u: full-name, email;")
+            for i, JSON in enumerate(users):
+                print(f"User #{i + 1}: {JSON}")
+
+
+def insert_new_user(driver):
+    print("\nRequest #2: Add new user with full-name and email")
+    with driver.session(DB_NAME, SessionType.DATA) as data_session:
+        with data_session.transaction(TransactionType.WRITE) as write_tx:
+            response = write_tx.query.insert(
+                "insert $p isa person, has full-name 'Jack Keeper', has email 'jk@vaticle.com';")
+            for res in response:
+                print(res.get("_0").as_attribute().get_value() + " " + res.get("_1").as_attribute().get_value())
+
+
+def get_files_by_user(driver, name, inference=False):
+    if not inference:
+        print("\nRequest #3: Find all files that user with selected name has access to view")
+        options = TypeDBOptions()
     else:
-        print("Setup failed. Terminating.")
+        print("\nRequest #4: Find all files that user with selected name has access to view (with inference)")
+        options = TypeDBOptions(infer=True)
+    with driver.session(DB_NAME, SessionType.DATA) as data_session:
+        with data_session.transaction(TransactionType.WRITE, options) as read_tx:
+            matched_users = list(read_tx.query.get(f"match $p isa person, has full-name '{name}'; get;"))
+            if len(matched_users) > 1:
+                print("Found more than one user with tha name")
+                return False
+            elif len(matched_users) == 1:
+                response = list(read_tx.query.get(f"""
+                                                match
+                                                $u isa user, has full-name '{name}';
+                                                $p($u, $pa) isa permission;
+                                                $o isa object, has path $fp;
+                                                $pa($o, $va) isa access;
+                                                $va isa action, has name 'view_file';
+                                                get $fp; sort $fp asc;
+                                                """))
+            else:
+                response = list(read_tx.query.get(f"""
+                                                match
+                                                $u isa user, has full-name $fn;
+                                                $fn contains '{name}';
+                                                $p($u, $pa) isa permission;
+                                                $o isa object, has path $fp;
+                                                $pa($o, $va) isa access;
+                                                $va isa action, has name 'view_file';
+                                                get $fp; sort $fp asc;
+                                                """))
+            for i, res in enumerate(response):
+                print(f"File #{i + 1}", res.get("fp").as_attribute().get_value())
 
+
+def main():
     with TypeDB.core_driver(SERVER_ADDR) as driver:
-        with driver.session(DB_NAME,SessionType.DATA) as data_session:
-            with data_session.transaction(TransactionType.READ) as read_txn:
-                print("\nRequest #1: User listing")
-                with open('queries/1-user-listing.tql', 'r') as data:
-                    typeql_fetch_query = data.read()
-                users = queries.fetch_users(data_session, typeql_fetch_query)
-                for i, JSON in enumerate(users):
-                    print(f"User # {i+1}: {JSON}")
+        setup.db_setup(driver, DB_NAME)
 
-                print("\nRequest #2: Files that Kevin Morrison has access to")
-                count, results = queries.get_query(data_session, 'queries/2-kevin-files.tql', ["fp"])
-                for result in results:
-                    print(result)
-                print("Files found:", count)
+        print("\nDemonstrating CRUD Operations:")
+        fetch_all_users(driver)
+        insert_new_user(driver)
+        get_files_by_user(driver, "Kevin Morrison")
+        get_files_by_user(driver, "Kevin Morrison", inference=True)
 
-                print("\nRequest #3: Files that Kevin Morrison has view access to (with inference)")
-                count, results = queries.get_query(data_session, 'queries/3-kevin-view-files.tql', ["fp"], inference=True)
-                for result in results:
-                    print(result)
-                print("Files found:", count)
+        # Update
+        # Delete
+        #
+        # print("\nSample Application Complete. Check the documentation for more details.")
 
 
+if __name__ == "__main__":
+    main()
 
-    #
-    #     print("\nRequest #4: Add a new file and a view access to it")
-    #     with session.transaction(TransactionType.WRITE) as transaction:  # Open a transaction to write
-    #         filename = "logs/" + datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + ".log"
-    #         typeql_insert_query = "insert $f isa file, has path '" + filename + "';"
-    #         transaction.query.insert(typeql_insert_query)  # Executing the query to insert the file
-    #         print("Inserted file:", filename)
-    #         typeql_insert_query = "match $f isa file, has path '" + filename + "'; " \
-    #                               "$vav isa action, has name 'view_file'; " \
-    #                               "insert ($vav, $f) isa access;"
-    #         transaction.query.insert(typeql_insert_query)  # Executing the second query in the same transaction
-    #         print("Added view access to the file.")
-    #         transaction.commit()  # commit transaction to persist changes
+#
+# print("\nRequest #2: Files that Kevin Morrison has access to")
+# count, results = queries.get_query(data_session, 'queries/2-kevin-files.tql', ["fp"])
+# for result in results:
+#     print(result)
+# print("Files found:", count)
+#
+# print("\nRequest #3: Files that Kevin Morrison has view access to (with inference)")
+# count, results = queries.get_query(data_session, 'queries/3-kevin-view-files.tql', ["fp"], inference=True)
+# for result in results:
+#     print(result)
+# print("Files found:", count)
+
+
+#
+#     print("\nRequest #4: Add a new file and a view access to it")
+#     with session.transaction(TransactionType.WRITE) as transaction:  # Open a transaction to write
+#         filename = "logs/" + datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + ".log"
+#         typeql_insert_query = "insert $f isa file, has path '" + filename + "';"
+#         transaction.query.insert(typeql_insert_query)  # Executing the query to insert the file
+#         print("Inserted file:", filename)
+#         typeql_insert_query = "match $f isa file, has path '" + filename + "'; " \
+#                               "$vav isa action, has name 'view_file'; " \
+#                               "insert ($vav, $f) isa access;"
+#         transaction.query.insert(typeql_insert_query)  # Executing the second query in the same transaction
+#         print("Added view access to the file.")
+#         transaction.commit()  # commit transaction to persist changes
--- setup.py
+++ setup.py
@@ -1,63 +1,71 @@
 from typedb.driver import TypeDB, SessionType, TransactionType
 
-DB_NAME = "iam"
+DB_NAME = "sample_app"
 SERVER_ADDR = "127.0.0.1:1729"
 
 
-def create_new_database(driver, db):
-    if driver.databases.contains(db):
-        driver.databases.get(DB_NAME).delete()  # Delete the database if it exists already
-    driver.databases.create(db)
-    if not driver.databases.contains(db):
+def create_new_database(driver, db_name):
+    if driver.databases.contains(db_name):
+        driver.databases.get(db_name).delete()  # Delete the database if it exists already
+    driver.databases.create(db_name)
+    if not driver.databases.contains(db_name):
         print("Database creation failed. Terminating...")
         exit()
 
 
 def db_schema_setup(schema_session, schema_file='iam-schema.tql'):
-    with schema_session.transaction(TransactionType.WRITE) as txn:
+    with schema_session.transaction(TransactionType.WRITE) as tx:
         with open(schema_file, 'r') as data:
             define_query = data.read()
-        txn.query.define(define_query)
-        txn.commit()
+        print("Defining schema", end="...")
+        tx.query.define(define_query)
+        tx.commit()
+        print("OK")
 
 
 def db_dataset_setup(data_session, data_file='iam-data-single-query.tql'):
-    with data_session.transaction(TransactionType.WRITE) as txn:
+    with data_session.transaction(TransactionType.WRITE) as tx:
         with open(data_file, 'r') as data:
             insert_query = data.read()
-        txn.query.insert(insert_query)
-        txn.commit()
+        print("Loading data", end="...")
+        tx.query.insert(insert_query)
+        tx.commit()
+        print("OK")
 
 
 def test_initial_database(data_session):
-    print("Testing the new database.")
-    with data_session.transaction(TransactionType.READ) as txn:  # Re-using a session to open a new transaction
+    with data_session.transaction(TransactionType.READ) as tx:
         test_query = "match $u isa user; get $u; count;"
-        response = txn.query.get_aggregate(test_query)
+        print("Testing the database", end="...")
+        response = tx.query.get_aggregate(test_query)
         result = response.resolve().as_value().as_long()
         if result == 3:
-            print("Test OK. Database setup complete.")
+            print("Passed")
             return True
         else:
-            print("Test failed with the following result:", result, " expected result: 3.")
+            print("Failed with the result:", result, "\n Expected result: 3.")
             return False
 
 
-def db_setup():
-    print("Setup")
-    with TypeDB.core_driver(SERVER_ADDR) as driver:
-        print("Connected to TypeDB Core server: ", SERVER_ADDR)
-        create_new_database(driver, DB_NAME)
-        with driver.session(DB_NAME, SessionType.SCHEMA) as session:
-            db_schema_setup(session)
-        with driver.session(DB_NAME, SessionType.DATA) as session:
-            db_dataset_setup(session)
-            if test_initial_database(session):
-                return True
-            else:
-                return False
+def db_setup(driver, db_name):
+    print(f"Setting up the database: {db_name}")
+    create_new_database(driver, db_name)
+    with driver.session(db_name, SessionType.SCHEMA) as session:
+        db_schema_setup(session)
+    with driver.session(db_name, SessionType.DATA) as session:
+        db_dataset_setup(session)
+        if test_initial_database(session):
+            print("Database setup complete.")
+            return True
+        else:
+            return False
+
+
+def main(driver):
+    if db_setup(driver, DB_NAME):
+        print("Setup complete.")
 
 
 if __name__ == "__main__":
-    if db_setup():
-        print("Setup complete.")
+    with TypeDB.core_driver(SERVER_ADDR) as driver:
+        main(driver)
