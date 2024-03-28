2a8cb58c7d8158e4a906ec1c4be9eb93289a23a2
Vladimir Izmalkov <vladimir@vaticle.com>
2024-03-28 11:55:42 +0000

    Add replace_database function

--- sample.py
+++ sample.py
@@ -34,6 +34,19 @@ def create_database(driver, db_name) -> bool:
 # end::create_new_db[]
 
 
+# tag::replace_db[]
+def replace_database(driver, db_name) -> bool:
+    print("Deleting an existing database", end="...")
+    driver.databases.get(db_name).delete()  # Delete the database if it exists already
+    print("OK")
+    if create_database(driver, db_name):
+        return True
+    else:
+        print("Failed to create a new database. Terminating...")
+        return False
+# end::replace_db[]
+
+
 # tag::db-schema-setup[]
 def db_schema_setup(schema_session, schema_file='iam-schema.tql'):
     with open(schema_file, 'r') as data:
@@ -79,11 +92,7 @@ def db_setup(driver, db_name, db_reset=False) -> bool:
     print(f"Setting up the database: {db_name}")
     if driver.databases.contains(db_name):
         if db_reset or (input("Found a pre-existing database. Do you want to replace it? (Y/N) ").lower() == "y"):
-            print("Deleting an existing database", end="...")
-            driver.databases.get(db_name).delete()  # Delete the database if it exists already
-            print("OK")
-            if not create_database(driver, db_name):
-                print("Failed to create a new database. Terminating...")
+            if not replace_database(driver, db_name):
                 return False
         else:
             print("Reusing an existing database.")
